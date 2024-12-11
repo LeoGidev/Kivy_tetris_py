920136e4f1d14b98764978d5f1934d14b41f0249
Leo Gimenez <leogimenez.dev@gmail.com>
2024-12-11 09:30:24 -0300

    se trabaja en clase principal

--- tetris_kivy.py
+++ tetris_kivy.py
@@ -58,3 +58,82 @@ class TetrisGame(Widget):
                         (new_y >= 0 and self.board[new_y][new_x])):
                         return True
         return False
+
+    def place_piece(self):
+        """Coloca la pieza en el tablero y elimina filas completas"""
+        for y, row in enumerate(self.current_piece):
+            for x, cell in enumerate(row):
+                if cell:
+                    self.board[self.current_pos[0] + y][self.current_pos[1] + x] = 1
+        self.clear_lines()
+        self.start_new_piece()
+
+    def clear_lines(self):
+        """Elimina filas completas del tablero"""
+        new_board = [row for row in self.board if any(cell == 0 for cell in row)]
+        while len(new_board) < BOARD_HEIGHT:
+            new_board.insert(0, [0 for _ in range(BOARD_WIDTH)])
+        self.board = new_board
+
+    def move_piece(self, dx, dy):
+        """Mueve la pieza actual"""
+        new_pos = [self.current_pos[0] + dy, self.current_pos[1] + dx]
+        if not self.check_collision(self.current_piece, new_pos):
+            self.current_pos = new_pos
+
+    def rotate_piece(self):
+        """Rota la pieza actual"""
+        new_piece = [list(row) for row in zip(*self.current_piece[::-1])]
+        if not self.check_collision(new_piece, self.current_pos):
+            self.current_piece = new_piece
+
+    def on_key_down(self, instance, key, *args):
+        """Control de teclado"""
+        if self.game_over:
+            return
+        if key == 276:  # Flecha izquierda
+            self.move_piece(-1, 0)
+        elif key == 275:  # Flecha derecha
+            self.move_piece(1, 0)
+        elif key == 274:  # Flecha abajo
+            self.move_piece(0, 1)
+        elif key == 273:  # Flecha arriba
+            self.rotate_piece()
+
+    def update(self, dt):
+        """Actualización periódica del juego"""
+        if self.game_over:
+            return
+        if not self.check_collision(self.current_piece, [self.current_pos[0] + 1, self.current_pos[1]]):
+            self.current_pos[0] += 1
+        else:
+            self.place_piece()
+        self.draw_board()
+
+    def draw_board(self):
+        """Dibuja el tablero y la pieza actual"""
+        self.canvas.clear()
+        with self.canvas:
+            # Dibuja las piezas fijas en el tablero
+            for y, row in enumerate(self.board):
+                for x, cell in enumerate(row):
+                    if cell:
+                        Color(0, 1, 0)
+                        Rectangle(pos=(x * GRID_SIZE, (BOARD_HEIGHT - y - 1) * GRID_SIZE),
+                                  size=(GRID_SIZE, GRID_SIZE))
+
+            # Dibuja la pieza actual
+            for y, row in enumerate(self.current_piece):
+                for x, cell in enumerate(row):
+                    if cell:
+                        Color(1, 0, 0)
+                        Rectangle(pos=((self.current_pos[1] + x) * GRID_SIZE,
+                                       (BOARD_HEIGHT - self.current_pos[0] - y - 1) * GRID_SIZE),
+                                  size=(GRID_SIZE, GRID_SIZE))
+
+class TetrisApp(App):
+    def build(self):
+        return TetrisGame()
+
+if __name__ == '__main__':
+    TetrisApp().run()
