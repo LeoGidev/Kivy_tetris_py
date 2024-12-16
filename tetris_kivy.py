f249db64f4eab8419cd308fb9cb25011cce2f08a
Leo Gimenez <leogimenez.dev@gmail.com>
2024-12-16 09:31:14 -0300

    se trabaja en detalles de imagen

--- tetris_kivy.py
+++ tetris_kivy.py
@@ -186,8 +186,7 @@ class TetrisApp(App):
         root = FloatLayout()
         info_panel = BoxLayout(orientation='vertical', size_hint=(0.3, 1), pos_hint={"right": 1})
         score_label = Label(text="Score: 0", font_size=20, color=(1, 1, 0, 1))
-        Color(0.1, 0.1, 0.1, 1)  # Fondo gris oscuro
-        rect = Rectangle()
+        level_label = Label(text="Level: 1", font_size=20, color=(1, 1, 0, 1))
 
         # Vincular dinámicamente las propiedades del rectángulo con las del info_panel
         def update_rect(instance, value):
